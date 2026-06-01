2d01a5a8a22245f8ce18fae01581c8e29ef17414
Arthur Assis Garcia <arthurassisgarcia@gmail.com>
2026-05-31 22:10:56 -0300

    Atualization 31.05.26

--- bd/src/main/java/dc/unifacef/bd/model/Produto.java
+++ bd/src/main/java/dc/unifacef/bd/model/Produto.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name="produtos")
 public class Produto {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column (nullable = false)
@@ -14,5 +14,37 @@ public class Produto {
     @Column (nullable = false)
     private Double preco;
     @Column (length = 500)
-    private String descricao;
+    private String descrico;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescrico() {
+        return descrico;
+    }
+
+    public void setDescrico(String descrico) {
+        this.descrico = descrico;
+    }
 }
--- bd/src/main/java/dc/unifacef/bd/service/ProdutoService.java
+++ bd/src/main/java/dc/unifacef/bd/service/ProdutoService.java
@@ -1,5 +1,4 @@
 package dc.unifacef.bd.service;
-
 import dc.unifacef.bd.model.Produto;
 import dc.unifacef.bd.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
@@ -10,27 +9,46 @@ import java.util.Optional;
 @Service
 public class ProdutoService {
 
-    // a injeção de dependencia sera pelo construtor
+    // a injeção de dependência será pelo construtor
     private ProdutoRepository repo;
 
     public ProdutoService(ProdutoRepository repo){
+
         this.repo = repo;
     }
     // listar os produtos da tabela produtos
-    public List<Produto> Lista(){
-        return repo.findAll();
+    public List<Produto> listar(){
+
+        return repo.findAll(); // select * from produtos
     }
-    // listar os produtos da tabela produtos por id
-    public Optional<Produto> BuscarPorId(Long id){
-        return repo.findById(id);
-    }
-    //remove o produto da tabela produtos
-    public boolean remove(Long id){
-        if(repo.existsById(id)) {
-            repo.deleteById(id);
-            return true; // remove o produto
-        }
-        return false; // nao remove
+    // listar os produtos da tabela produtos por um id
+    public Optional<Produto> buscarPorId(Long id){
+        // select * from produtos where id = id
+        return repo.findById(id); // pode ou não retornar um produto
     }
 
+    // remove um produto da tabela produtos
+    public boolean remove(Long id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return true; // remove
+        }
+        return false; // não remove
+    }
+    // atualiza um produto por um id e o produto alterado
+    public Produto atualiza(Long id, Produto alterado){
+        if (repo.existsById(id)){
+            // produto existe para atualizar
+            alterado.setId(id);
+            return repo.save(alterado); // como alterado tem id, ele faz update
+        }
+        return null; // produto não existe
+    }
+    // cria ou insere o produto no banco
+    public Produto salva(Produto produto){
+        if (produto.getDescrico().equals("robo")){
+            return null;
+        }
+        return repo.save(produto); // como produto não tem id, ele faz insert
+    }
 }
